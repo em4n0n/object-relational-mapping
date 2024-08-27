f3221c800416752d2317ab30fdc70bb46b0c3891
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-26 21:12:15 -0700

    Change format of models

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Customer(models.Model):
-    name = models.CharField(max_length=255)
-    
-class Vehicle(models.Model):
-    name = models.CharField(max_length=255)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='Vehicle')
+class Customer(models.Model): 
+    name = models.CharField(max_length=255) 
+
+class Vehicle(models.Model): 
+    name = models.CharField(max_length=255) 
+    model = models.CharField(max_length=100)
+    customer = models.ForeignKey( 
+        Customer, 
+        on_delete=models.CASCADE, 
+        related_name='Vehicle' 
+    ) 
