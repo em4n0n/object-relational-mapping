b8039ef8d060bf19c482fb8dc8be1822dcbd0524
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-27 21:08:18 -0700

    Update models.py

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -6,7 +6,6 @@ class Customer(models.Model):
 
 class Vehicle(models.Model): 
     name = models.CharField(max_length=255) 
-    model = models.CharField(max_length=100)
     customer = models.ForeignKey( 
         Customer, 
         on_delete=models.CASCADE, 
